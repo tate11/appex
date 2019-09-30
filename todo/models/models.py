d400cf4e256d56e2cdb29d133f6f51076b0e6a28
unknown <nguyenductu@gmail.com>
2019-09-30 22:57:49 +0700

    ok

--- todo/models/models.py
+++ todo/models/models.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import datetime
 
 class Todo(models.Model):
     _name = 'todo.todo'
+    _rec_name = 'description'
     #Draft --> Waiting for Approval --> Approved or Rejected.
     state = fields.Selection([
         ('draft', 'Draft'),
         ('waiting_for_approval', 'Waiting for Approval'),
         ('approved', 'Approved'),
         ('rejected', 'Rejected'),
-        ('cancel', 'Cancel')
+#         ('cancel', 'Cancel')
         ], default='draft')
     description = fields.Text()
-    created_date = fields.Date()
-    deadline = fields.Date()
-    responsible_id = fields.Many2one('res.users')
+    created_date = fields.Date(default=fields.Date.context_today)
+    def offset_date(self):
+        today = datetime.date.today()
+        print ('today',today,type(today))
+        the_day = today + datetime.timedelta(days=7)
+        return the_day
+    deadline = fields.Date(default= offset_date)
+    responsible_id = fields.Many2one('res.users',default=lambda self:self.env.uid)
     manager_id = fields.Many2one('res.users')
     
 
@@ -31,10 +38,10 @@ class Todo(models.Model):
     @api.multi
     def action_rejected(self):
         self.state = 'rejected'
-    @api.multi
-    def action_cancel(self):
-        self.state = 'cancel'    
-        
+#     @api.multi
+#     def action_cancel(self):
+#         self.state = 'cancel'    
+#         
     
     
 
